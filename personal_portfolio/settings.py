45851d420af1ccd32ca4e813d851bb54ba11c48a
kBarbz <kbarbz1995@gmail.com>
2019-12-14 20:37:18 +0800

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -125,11 +125,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
